dfdd972914278b9f7511cc33bed44aa391bb969a
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2025-04-14 23:33:36 +0600

    update vite rule

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["*"],
+    // allowedHosts: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
